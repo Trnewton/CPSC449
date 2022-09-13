4b091e50a77f96937d55d0ec3e65fe9d9f521ff1
trnewton <thomas.newton@ucalgary.ca>
2022-09-12 23:40:18 -0600

    clean

--- Exercises/A1/Ex1.hs
+++ Exercises/A1/Ex1.hs
@@ -18,9 +18,6 @@ module Ex1 where
 import Ex1Types
 
 -- This imports some standard library functions for you to use.
--- This imports some standard library functions for you to use.
--- This imports some standard library functions for you to use.
--- This imports some standard library functions for you to use.
 import Prelude (Int, Float, Integer, Eq, Ord, Bool (..), String, otherwise, abs, (+), (-), subtract, (*), (/), (==), (/=), (<), (<=), (>), (>=), (||), (&&), rem, mod, div, quot, max, min, fromIntegral, undefined, error, show)
 -- This includes helpful functions for debugging.
 import Debug.Trace
