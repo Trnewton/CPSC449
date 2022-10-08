87b8a6c35b2b1c7586a157fcb285db8111320bec
Trnewton <thomas.newton@ucalgary.ca>
2022-10-07 22:42:41 -0600

    monke

--- Exercises/A2/Ex2.hs
+++ Exercises/A2/Ex2.hs
@@ -412,7 +412,7 @@ strong_balance = balance
 --         rot_left (Node lt a (Node lrt ar rrt)) = (Node (Node lt a lrt) ar rrt)
 
 ordTree2List :: STree a -> [a]
-ordTree2List = foldavl (\ll a rl -> (ordTree2List ll).(\x -> a:x).(ordTree2List rl)) id
+ordTree2List = foldavl (\ll a rl -> (ordTree2List ll).(\x -> a:x).(ordTree2List rl) ) id
 
 balance :: STree a -> STree a
 balance Leaf = Leaf
@@ -425,6 +425,7 @@ balance t = balance' (ordTree2List t)
                 midIdx = quot (length lst) 2
                 left = take midIdx lst
                 right = drop (midIdx+1) lst
+
 
 
 numBelow :: STree a -> STree Int
